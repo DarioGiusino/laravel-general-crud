241850e8fad2d90d8cb56b44d92d139bdeca6c5e
domenicatonziello <domenica.tonziello10@gmail.com>
2023-03-06 16:09:07 +0100

    add create() and store()

--- app/Http/Controllers/ToolController.php
+++ app/Http/Controllers/ToolController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tool;
 use Illuminate\Http\Request;
+use SebastianBergmann\GlobalState\Restorer;
 
 class ToolController extends Controller
 {
-    //
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('tools.create');
+    }
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $new_tool = new Tool();
+        $new_tool->fill($data);
+        $new_tool->save();
+
+        return to_route('tools.index');
+    }
 }
