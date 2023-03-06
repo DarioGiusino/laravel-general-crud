fb7aa05e19ffa9163f51fd79c5254771cfaf6e7a
DarioGiusino <dario.giusino@gmail.com>
2023-03-06 12:13:28 +0100

    base layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,11 +5,23 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>{{ env('APP_NAME') }} | @yield('title')</title>
+  {{-- cdns --}}
+  @yield('cdns')
+
+  @vite('resources/js/app.js')
 </head>
 
 <body>
+  <header>
+    @yield('header')
+  </header>
 
+  <main>
+    @yield('content')
+  </main>
+
+  @yield('scripts')
 </body>
 
 </html>
