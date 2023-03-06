55f1edfeffa0238d6c26a7feba64be8822508fb4
DarioGiusino <dario.giusino@gmail.com>
2023-03-06 12:57:47 +0100

    added protected variable in model

--- app/Models/Tool.php
+++ app/Models/Tool.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Tool extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'image_url', 'description', 'category', 'release_year', 'latest_version', 'download_link', 'supported_os', 'vote'];
 }
