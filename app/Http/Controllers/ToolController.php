5110f64a5eb1af71a13ccb4c7797a382d15ffa65
PinoMacri <pinonegozio1@outlook.it>
2023-03-06 15:39:07 +0100

    added function desotry

--- app/Http/Controllers/ToolController.php
+++ app/Http/Controllers/ToolController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Tool;
 
 class ToolController extends Controller
 {
-    //
+    public function destroy(Tool $tool)
+    {
+        $tool->delete();
+        return to_route("tools.index")->with("delete", "$tool->name è stato eliminato con successo");
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::delete("/tools/{tools}", [ToolController::class,"destroy"])->name("tools.destroy");
