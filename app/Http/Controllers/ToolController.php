84c3605af7565da92986fade834c7f60114292d9
NicolaPlacentino <nyko0921@gmail.com>
2023-03-06 15:29:23 +0100

    comlted show

--- app/Http/Controllers/ToolController.php
+++ app/Http/Controllers/ToolController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tool;
 use Illuminate\Http\Request;
 
 class ToolController extends Controller
 {
-    //
+    public function show(string $id)
+    {
+        $tool = Tool::findOrFail($id);
+        return view("tools.show", compact("tool"));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ToolController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/tools/{tool}', [ToolController::class, 'show'])->name('tools.show');
