02294f2692b0f80f7c352a065dc5a798bdc338f4
DarioGiusino <dario.giusino@gmail.com>
2023-03-06 17:03:10 +0100

    create fix

--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,11 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 
 // tools index route
 Route::get('/tools', [ToolController::class, 'index'])->name('tools.index');
+// route per creazione (1 step)
+Route::get('/tools/create', [ToolController::class, 'create'])->name('tools.create');
+// route per creazione (2 step)
+Route::post('/tools', [ToolController::class, 'store'])->name('tools.store');
 Route::get('/tools/{tool}', [ToolController::class, 'show'])->name('tools.show');
 Route::delete("/tools/{tool}", [ToolController::class, "destroy"])->name("tools.destroy");
 Route::get('/tools/{tool}/edit', [ToolController::class, 'edit'])->name('tools.edit');
 Route::put('/tools/{tool}', [ToolController::class, 'update'])->name('tools.update');
-// route per creazione (1 step)
-Route::get('tools/create', [ToolController::class, 'create'])->name('tools.create');
-// route per creazione (2 step)
-Route::post('/tools', [ToolController::class, 'store'])->name('tools.store');
