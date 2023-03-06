465934c5900eb5e716cfc2199cd70f43d15059f3
DarioGiusino <dario.giusino@gmail.com>
2023-03-06 15:09:59 +0100

    defined index method in toolcontroller

--- app/Http/Controllers/ToolController.php
+++ app/Http/Controllers/ToolController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tool;
 use Illuminate\Http\Request;
 
 class ToolController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tools = Tool::all();
+        return view('home', compact('tools'));
+    }
 }
