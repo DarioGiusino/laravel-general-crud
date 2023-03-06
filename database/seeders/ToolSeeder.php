71a1b9cbbb6ebea6a8d1b51296f2bdf956b92098
DarioGiusino <dario.giusino@gmail.com>
2023-03-06 12:56:29 +0100

    defined tool seeder

--- database/seeders/ToolSeeder.php
+++ database/seeders/ToolSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Tool;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,12 @@ class ToolSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $tools = config('tools');
+
+        foreach ($tools as $tool) {
+            $new_tool = new Tool();
+            $new_tool->fill($tool);
+            $new_tool->save();
+        }
     }
 }
