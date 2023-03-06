d9e6664bc3572bd4c4c52f4bbe72293a14d71a16
DarioGiusino <dario.giusino@gmail.com>
2023-03-06 15:09:09 +0100

    defined tools index route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ToolController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+// tools index route
+Route::get('/tools', [ToolController::class, 'index'])->name('tools.index');
